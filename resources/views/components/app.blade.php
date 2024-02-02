a2c579ac5e94fb60c9f4006fd4c269b45cda1bbe
Ant <dev.anthonyterrell@gmail.com>
2024-02-02 08:15:30 -0600

    Uses app.name config for title

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name') }}</title>
 
         <link rel="apple-touch-icon" href="https://assets.nhle.com/logos/nhl/svg/CHI_light.svg">
 
