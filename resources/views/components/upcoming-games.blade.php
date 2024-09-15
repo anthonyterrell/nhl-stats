c6966b0136e2726de2a4f88e299353c5ddb8bb3b
Anthony Terrell <dev.anthonyterrell@gmail.com>
2024-09-15 17:04:11 -0500

    Adds handling for preseason

--- resources/views/components/upcoming-games.blade.php
+++ resources/views/components/upcoming-games.blade.php
@@ -1,14 +1,18 @@
 <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-  <div class="px-4 py-5 sm:px-6 text-center">Upcoming Games</div>
+  <div class="px-4 py-5 sm:px-6 text-center font-semibold">Upcoming Games</div>
   <div class="px-4 py-5 sm:p-6">
     <ul role="list" class="divide-y divide-gray-200">
         @foreach($games as $game)
             <li class="flex justify-between gap-x-6 py-1 sm:py-2">
                 <div class="flex flex-col min-w-0 gap-x-4">
                     <img src="{{ $game->awayTeam->logo}}" class="h-12 w-12">
-                    <p class="text-xs">
-                        {{ $game->awayTeam->teamStatistics?->wins ?? 0 }}-{{ $game->awayTeam->teamStatistics?->losses ?? 0}}-{{ $game->awayTeam->teamStatistics?->otLosses ?? 0}}
-                    </p>
+                    @if($game->gameType == 1)
+                        <p class="text-xs">Preseason</p>
+                    @else
+                        <p class="text-xs">
+                            {{ $game->awayTeam->teamStatistics?->wins ?? 0 }}-{{ $game->awayTeam->teamStatistics?->losses ?? 0}}-{{ $game->awayTeam->teamStatistics?->otLosses ?? 0}}
+                        </p>
+                    @endunless
                 </div>
 
                 <div class="flex flex-col items-center justify-center">
@@ -19,9 +23,13 @@
                 <div class="shrink-0 flex flex-col items-end">
                     <img src="{{ $game->homeTeam->logo}}" class="h-12 w-12">
 
-                    <p class="text-xs">
-                        {{ $game->homeTeam->teamStatistics?->wins ?? 0 }}-{{ $game->homeTeam->teamStatistics?->losses ?? 0 }}-{{ $game->homeTeam->teamStatistics?->otLosses ?? 0 }}
-                    </p>
+                    @if($game->gameType == 1)
+                        <p class="text-xs">Preseason</p>
+                    @else
+                        <p class="text-xs">
+                            {{ $game->homeTeam->teamStatistics?->wins ?? 0 }}-{{ $game->homeTeam->teamStatistics?->losses ?? 0 }}-{{ $game->homeTeam->teamStatistics?->otLosses ?? 0 }}
+                        </p>
+                    @endunless
                 </div>
             </li>
         @endforeach
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,17 @@
 <x-app>
     <div class="w-full md:max-w-2xl mx-auto p-4">
+        <h1 class="text-2xl text-center mb-8 font-bold"> {{ currentSeason() . ' Season' }}</h1>
+
         <x-upcoming-games :games="$schedule->take(3)" />
+
+        @if(inPreSeason())
+            <div class="mt-8">
+                <p class="text-sm text-gray-500">
+                    <span class="font-bold">These are last seasons standings</span>.
+                    Preseason games are not counted towards the regular season standings.
+                </p>
+            </div>
+        @endif
 
         <x-league-standings :standings="$standings" />
     </div>
