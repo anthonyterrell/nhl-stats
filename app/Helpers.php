0459df9c8aed419c6f7b3a11d9c7007328cad4de
Anthony Terrell <dev.anthonyterrell@gmail.com>
2024-09-15 17:03:13 -0500

    Adds currentSeason() helper

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,7 +1,25 @@
 <?php
 
 use App\Api\NHL\NHLApi;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
+
+if (!function_exists('currentSeason')) {
+    function currentSeason()
+    {
+        return Cache::remember('currentSeason', 86400, function () {
+            $season =  collect(app(NHLApi::class)->fullScheduleFor(myTeam())->games)->first()?->season;
+
+            if(!$season || now()->gt(new Carbon('first day of june'))) {
+                return now()->format('Y') . '-' . now()->addYear()->format('Y');
+            }
+
+            $startingYear = Carbon::createFromFormat('Y',substr($season, 0, 4));
+
+            return $startingYear->format('Y') . '-' . $startingYear->addYear()->format('Y');
+         });
+    }
+}
 
 if (!function_exists('myTeam')) {
     function myTeam()
